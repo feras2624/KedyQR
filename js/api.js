39c231ed043b14a392afc83fe3af94ea3c1e1a9e
Firas <170180050+feras2624@users.noreply.github.com>
2026-07-08 19:31:48 +0300

    v0.2 change the root of json files

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 export async function loadMenu(cafe) {
 
     const response =
-        await fetch(`/cafes/${cafe}/menu.json`);
+        await fetch(`cafes/${cafe}/menu.json`);
 
     if (!response.ok)
         throw new Error("Menu not found.");
