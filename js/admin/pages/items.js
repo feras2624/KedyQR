a86a8f91acbcebe934ae72c39c6af41104cf527e
Firas <170180050+feras2624@users.noreply.github.com>
2026-07-11 21:51:58 +0300

    bug fixes

--- js/admin/pages/items.js
+++ js/admin/pages/items.js
@@ -355,8 +355,8 @@ const image = ImagePicker(
 
                     item.category = category.value;
 
-                    //item.image = image.value;
-
+                    item.image = image.value;
+                    console.log(JSON.stringify(state.menu,null,2));
                     markDirty();
 
                     closeModal();
