1fa7c824ce1ecbb067db8c488e93b3b84e72a041
Firas <170180050+feras2624@users.noreply.github.com>
2026-07-14 18:06:28 +0300

    from rw to b64

--- js/api.js
+++ js/api.js
@@ -1,16 +1,46 @@
 import { DATA_URL } from "./state.js"
 
 
+// export async function loadMenu(cafe) {
+
+//     const response =
+//     await fetch(`${DATA_URL}/${cafe}/menu.json?v=${Date.now()}`, {
+//     cache: "no-store"
+//     });
+
+//     if (!response.ok)
+//         throw new Error("Menu not found.");
+
+//     return await response.json();
+
+// }
+
+
 export async function loadMenu(cafe) {
 
-    const response =
-    await fetch(`${DATA_URL}/${cafe}/menu.json?v=${Date.now()}`, {
-    cache: "no-store"
-    });
+    const response = await fetch(
+
+        `https://api.github.com/repos/feras2624/KedyQR-Data/contents/${cafe}/menu.json`,
+
+        {
+
+            cache: "no-store"
+
+        }
+
+    );
 
     if (!response.ok)
         throw new Error("Menu not found.");
 
-    return await response.json();
+    const data = await response.json();
+
+    const text = atob(
+
+        data.content.replace(/\n/g, "")
+
+    );
+
+    return JSON.parse(text);
 
 }
