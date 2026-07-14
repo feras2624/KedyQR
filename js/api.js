9ffbcf6cc4542e4bb22bc169cec0e9705476ac0e
Firas <170180050+feras2624@users.noreply.github.com>
2026-07-14 18:13:40 +0300

    from rw to b64 fix unicode

--- js/api.js
+++ js/api.js
@@ -1,46 +1,22 @@
-import { DATA_URL } from "./state.js"
-
-
-// export async function loadMenu(cafe) {
-
-//     const response =
-//     await fetch(`${DATA_URL}/${cafe}/menu.json?v=${Date.now()}`, {
-//     cache: "no-store"
-//     });
-
-//     if (!response.ok)
-//         throw new Error("Menu not found.");
-
-//     return await response.json();
-
-// }
-
 
 export async function loadMenu(cafe) {
 
-    const response = await fetch(
+    const response =
+    await fetch(`https://api.github.com/repos/feras2624/KedyQR-Data/contents/${cafe}/menu.json?v=${Date.now()}`,         {
 
-        `https://api.github.com/repos/feras2624/KedyQR-Data/contents/${cafe}/menu.json`,
+            headers: {
 
-        {
+                Accept: "application/vnd.github.raw"
+
+            },
 
             cache: "no-store"
 
-        }
-
-    );
+        });
 
     if (!response.ok)
         throw new Error("Menu not found.");
 
-    const data = await response.json();
-
-    const text = atob(
-
-        data.content.replace(/\n/g, "")
-
-    );
-
-    return JSON.parse(text);
+    return await response.json();
 
 }
